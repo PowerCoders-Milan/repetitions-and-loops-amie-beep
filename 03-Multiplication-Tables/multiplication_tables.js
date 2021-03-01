386cadc65702a0b81f0f866aafa94cd550a00e8f
amie senghore <amiesenghore1@gmail.com>
2021-03-01 14:20:20 +0100

    finishedMulti-tables

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,17 @@
 // write your code below
+
+console.log("Only with number")
+for(var i = 0; i <= 10; i++){
+    var num = 9;
+    var res = i * num;
+    console.log(i +" * " + num+ " = "+ res);
+}
+//function start
+console.log("Function starts with differnt number")
+function multiply(num){
+    for(var i = 0; i <= 10; i++){
+    var res = i * num;
+    console.log(i +" * " + num+ " = "+ res);
+    } 
+}
+multiply(8);
