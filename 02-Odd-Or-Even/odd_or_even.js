b2b58c56479162dccd72a8c8041e7e22015cdcfd
amie senghore <amiesenghore1@gmail.com>
2021-03-01 12:42:41 +0100

    finishedOdd-Even

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,7 +5,9 @@
   // check if the current is even or odd
 
   // report the result to the screen
-
+   
+  //This for loop
+  console.log("For loop")
   for (var step = 0; step < 21; step ++) {
     if (step % 2 === 0) {
       console.log(step + " is even");
@@ -15,3 +17,24 @@
       }
     }
   }
+
+  //while loop
+  console.log("While loop")
+  var num = 0;
+  while(num <= 20) {
+    if(num % 2 == 0)
+    console.log(num + " is even");
+    else console.log(num + " is odd");
+    num ++;
+  }
+
+  
+  // do while loop
+  console.log("Do while loop")
+  var number = 0;
+do{
+  if(number % 2 ==0) console.log(number+" is even");
+  else console.log(number+" is odd");
+  number++;
+
+}while(number <= 20);
